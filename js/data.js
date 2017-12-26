881035fbe1658f31cb18d86c79abb5c3f15b5d83
betsyvies <ing.betsyvies@gmail.com>
2017-12-26 01:57:26 -0500

    añadiendo estilos de las vistas y data

---
+++ js/data.js
@@ -0,0 +1,37 @@
+var data = {
+  'comida sana': {
+    'restaurant1': '../assets/images/restaurants/comida-sana/1.jpg',
+    'restaurant2': '../assets/images/restaurants/comida-sana/2.jpg',
+    'restaurant3': '../assets/images/restaurants/comida-sana/3.jpg',
+    'restaurant4': '../assets/images/restaurants/comida-sana/4.jpg',
+    'restaurant5': '../assets/images/restaurants/comida-sana/5.jpg',
+  },
+  'comida mexicana': {
+    'restaurant1': '../assets/images/restaurants/comida-mexicana/1.jpg',
+    'restaurant2': '../assets/images/restaurants/comida-mexicana/2.jpg',
+    'restaurant3': '../assets/images/restaurants/comida-mexicana/3.jpg',
+    'restaurant4': '../assets/images/restaurants/comida-mexicana/4.jpg',
+    'restaurant5': '../assets/images/restaurants/comida-mexicana/5.jpg',
+  },
+  'comida china': {
+    'restaurant1': '../assets/images/restaurants/comida-china/1.jpg',
+    'restaurant2': '../assets/images/restaurants/comida-china/2.jpg',
+    'restaurant3': '../assets/images/restaurants/comida-china/3.jpg',
+    'restaurant4': '../assets/images/restaurants/comida-china/4.jpg',
+    'restaurant5': '../assets/images/restaurants/comida-china/5.jpg',
+  },
+  'comida rapida': {
+    'restaurant1': '../assets/images/restaurants/comida-rapida/1.jpg',
+    'restaurant2': '../assets/images/restaurants/comida-rapida/2.jpg',
+    'restaurant3': '../assets/images/restaurants/comida-rapida/3.jpg',
+    'restaurant4': '../assets/images/restaurants/comida-rapida/4.jpg',
+    'restaurant5': '../assets/images/restaurants/comida-rapida/5.jpg',
+  },
+  'comida criolla': {
+    'restaurant1': '../assets/images/restaurants/comida-criolla/1.jpg',
+    'restaurant2': '../assets/images/restaurants/comida-criolla/2.jpg',
+    'restaurant3': '../assets/images/restaurants/comida-criolla/3.jpg',
+    'restaurant4': '../assets/images/restaurants/comida-criolla/4.jpg',
+    'restaurant5': '../assets/images/restaurants/comida-criolla/5.jpg',
+  }
+};
